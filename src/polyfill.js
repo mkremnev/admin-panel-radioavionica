fe0e8d2924e287f0ed39a65042097dcbdef31a03
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-10-13 22:33:35 +0300

    Prettier formatting

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,7 +1,7 @@
 /*
-* required polyfills
-*/
-import "core-js";
+ * required polyfills
+ */
+import 'core-js';
 import 'core-js/features/set/map';
 import 'core-js/features/map';
 
@@ -31,17 +31,25 @@ import 'core-js/features/map';
 
 // CustomEvent() constructor functionality in IE9, IE10, IE11
 (function () {
+	if (typeof window.CustomEvent === 'function') return false;
 
-  if ( typeof window.CustomEvent === "function" ) return false
+	function CustomEvent(event, params) {
+		params = params || {
+			bubbles: false,
+			cancelable: false,
+			detail: undefined,
+		};
+		const evt = document.createEvent('CustomEvent');
+		evt.initCustomEvent(
+			event,
+			params.bubbles,
+			params.cancelable,
+			params.detail,
+		);
+		return evt;
+	}
 
-  function CustomEvent ( event, params ) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined }
-    var evt = document.createEvent( 'CustomEvent' )
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail )
-    return evt
-  }
+	CustomEvent.prototype = window.Event.prototype;
 
-  CustomEvent.prototype = window.Event.prototype
-
-  window.CustomEvent = CustomEvent
-})()
+	window.CustomEvent = CustomEvent;
+})();
