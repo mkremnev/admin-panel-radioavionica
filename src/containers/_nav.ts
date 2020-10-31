36ff53114baebad88f49b8dedb1472619467e976
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-10-31 21:41:12 +0300

    Edited navLink for Military Lists

--- src/containers/_nav.ts
+++ src/containers/_nav.ts
@@ -17,102 +17,14 @@ export default [
 		icon: 'cil-description',
 	},
 	{
-		_tag: 'CSidebarNavItem',
-		name: 'Typography',
-		to: '/theme/typography',
-		icon: 'cil-pencil',
-	},
-	{
 		_tag: 'CSidebarNavTitle',
-		_children: ['Components'],
+		_children: ['Войсковые части'],
 	},
 	{
-		_tag: 'CSidebarNavDropdown',
-		name: 'Base',
-		route: '/base',
-		icon: 'cil-puzzle',
-		_children: [
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Breadcrumb',
-				to: '/base/breadcrumbs',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Cards',
-				to: '/base/cards',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Carousel',
-				to: '/base/carousels',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Collapse',
-				to: '/base/collapses',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Forms',
-				to: '/base/forms',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Jumbotron',
-				to: '/base/jumbotrons',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'List group',
-				to: '/base/list-groups',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Navs',
-				to: '/base/navs',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Navbars',
-				to: '/base/navbars',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Pagination',
-				to: '/base/paginations',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Popovers',
-				to: '/base/popovers',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Progress',
-				to: '/base/progress-bar',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Switches',
-				to: '/base/switches',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Tables',
-				to: '/base/tables',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Tabs',
-				to: '/base/tabs',
-			},
-			{
-				_tag: 'CSidebarNavItem',
-				name: 'Tooltips',
-				to: '/base/tooltips',
-			},
-		],
+		_tag: 'CSidebarNavItem',
+		name: 'Список частей',
+		to: '/military_units/lists',
+		icon: 'cil-home',
 	},
 	{
 		_tag: 'CSidebarNavDropdown',
