d3457bcfb3542f8dbe95f3b6be2dc7ad77b3a5d1
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-11-05 22:06:08 +0300

    Created saga for component Lists

---
+++ src/views/military_units/lists/saga.ts
@@ -0,0 +1,16 @@
+import { actions } from './reducer';
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { getListsUnits } from '../../../api/getDefects';
+
+function* fetchDefectsAsync() {
+	try {
+		const data = yield call(getListsUnits);
+		yield put(actions.requestedListsSuccess(data));
+	} catch (error) {
+		yield put(actions.requestedListsFailure());
+	}
+}
+
+export function* watchRequestedLists() {
+	yield takeEvery(actions.requestedLists.type, fetchDefectsAsync);
+}
