f4c93b3b93d0d62d1e9ec19cb521ca99dd106000
mkremnev <m.kremnev@netlinux.ru>
2020-12-08 22:29:24 +0300

    Added mapping DB entity

--- backend/public/config/common/doctrine.php
+++ backend/public/config/common/doctrine.php
@@ -1,62 +1,1 @@
-<?php
-
-declare(strict_types=1);
-
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-use Doctrine\Common\Cache\ArrayCache;
-use Psr\Container\ContainerInterface;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Common\Cache\FilesystemCache;
-use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
-
-return [
-    EntityManagerInterface::class => function (ContainerInterface $container): EntityManagerInterface {
-        /**
-         * @psalm-suppress MixedArrayAccess
-         * @psalm-var array{
-         *     metadata_dirs:array,
-         *     dev_mode:bool,
-         *     proxy_dir:string,
-         *     cache_dir:?string,
-         *     types:array<string,string>,
-         *     subscribers:string[],
-         *     connection:array
-         * } $settings
-         */
-        $settings = $container->get('config')['doctrine'];
-
-        $config = Setup::createAnnotationMetadataConfiguration(
-            $settings['metadata_dirs'],
-            $settings['dev_mode'],
-            $settings['proxy_dir'],
-            $settings['cache_dir'] ? new FilesystemCache($settings['cache_dir']) : new ArrayCache(),
-            false
-        );
-
-        $config->setNamingStrategy(new UnderscoreNamingStrategy());
-
-        return EntityManager::create(
-            $settings['connection'],
-            $config
-        );
-    },
-
-    'config' => [
-        'doctrine' => [
-            'dev_mode' => false,
-            'cache_dir' => __DIR__ . '/../../var/cache/doctrine/cache',
-            'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',
-            'connection' => [
-                'driver' => 'pdo_pgsql',
-                'host' => getenv('DB_HOST'),
-                'user' => getenv('DB_USER'),
-                'password' => getenv('DB_PASSWORD'),
-                'dbname' => getenv('DB_NAME'),
-                'charset' => 'utf-8'
-            ],
-            'subscribers' => [],
-            'metadata_dirs' => [],
-        ],
-    ],
-];
+<?phpdeclare(strict_types=1);namespace App;use Doctrine\ORM\Tools\Setup;use Doctrine\ORM\EntityManager;use Doctrine\Common\Cache\ArrayCache;use Psr\Container\ContainerInterface;use Doctrine\ORM\EntityManagerInterface;use Doctrine\Common\Cache\FilesystemCache;use Doctrine\DBAL\Types\Type;use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;return [    EntityManagerInterface::class => function (ContainerInterface $container): EntityManagerInterface {        /**         * @psalm-suppress MixedArrayAccess         * @psalm-var array{         *     metadata_dirs:array,         *     dev_mode:bool,         *     proxy_dir:string,         *     cache_dir:?string,         *     types:array<string,string>,         *     subscribers:string[],         *     connection:array         * } $settings         */        $settings = $container->get('config')['doctrine'];        $config = Setup::createAnnotationMetadataConfiguration(            $settings['metadata_dirs'],            $settings['dev_mode'],            $settings['proxy_dir'],            $settings['cache_dir'] ? new FilesystemCache($settings['cache_dir']) : new ArrayCache(),            false        );        $config->setNamingStrategy(new UnderscoreNamingStrategy());        foreach ($settings['types'] as $name => $class) {            if (!Type::hasType($name)) {                Type::addType($name, $class);            }        }        return EntityManager::create(            $settings['connection'],            $config        );    },    'config' => [        'doctrine' => [            'dev_mode' => false,            'cache_dir' => __DIR__ . '/../../var/cache/doctrine/cache',            'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',            'connection' => [                'driver' => 'pdo_pgsql',                'host' => getenv('DB_HOST'),                'user' => getenv('DB_USER'),                'password' => getenv('DB_PASSWORD'),                'dbname' => getenv('DB_NAME'),                'charset' => 'utf-8'            ],            'subscribers' => [],            'metadata_dirs' => [                __DIR__ . '/../../src/Auth/Entity'            ],            'types' => [                Auth\Entity\User\IdType::NAME => Auth\Entity\User\IdType::class,                Auth\Entity\User\EmailType::NAME => Auth\Entity\User\EmailType::class,                Auth\Entity\User\RoleType::NAME => Auth\Entity\User\RoleType::class,                Auth\Entity\User\StatusType::NAME => Auth\Entity\User\StatusType::class,            ]        ],    ],];
