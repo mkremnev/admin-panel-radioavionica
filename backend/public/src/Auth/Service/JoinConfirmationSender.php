de2230f170da852f49fd72f307a555707663b935
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-12-21 18:30:12 +0300

    Created Twig templates for mail

--- backend/public/src/Auth/Service/JoinConfirmationSender.php
+++ backend/public/src/Auth/Service/JoinConfirmationSender.php
@@ -1,39 +1,1 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Auth\Service;
-
-use App\Auth\Entity\User\Email;
-use App\Auth\Entity\User\Token;
-use RuntimeException;
-use Swift_Mailer;
-use Swift_Message;
-
-use function http_build_query;
-
-class JoinConfirmationSender
-{
-    private Swift_Mailer $mailer;
-    private array $from;
-
-    public function __construct(Swift_Mailer $mailer, array $from)
-    {
-        $this->mailer = $mailer;
-        $this->from = $from;
-    }
-
-    public function send(Email $email, Token $token): void
-    {
-        $message = (new Swift_Message('Join confirmation'))
-            ->setFrom($this->from)
-            ->setTo($email->getValue())
-            ->setBody('/join/confirm?' . http_build_query([
-                'token' => $token->getValue()
-                ]));
-
-        if ($this->mailer->send($message) === 0) {
-            throw new RuntimeException('Unable to send mail');
-        }
-    }
-}
+<?phpdeclare(strict_types=1);namespace App\Auth\Service;use App\Auth\Entity\User\Email;use App\Auth\Entity\User\Token;use App\Frontend\FrontendUrlGenerator;use RuntimeException;use Swift_Mailer;use Swift_Message;use Twig\Environment;use function http_build_query;class JoinConfirmationSender{    private Swift_Mailer $mailer;    private FrontendUrlGenerator $frontend;    private Environment $twig;    public function __construct(Swift_Mailer $mailer, Environment $twig)    {        $this->mailer = $mailer;        $this->twig = $twig;    }    public function send(Email $email, Token $token): void    {        $message = (new Swift_Message('Join confirmation'))            ->setTo($email->getValue())            ->setBody($this->twig->render('auth/join/confirm.html.twig', ['token' => $token]), 'text/html');        if ($this->mailer->send($message) === 0) {            throw new RuntimeException('Unable to send mail');        }    }}
