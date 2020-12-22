d03862384536f1a0a131d37243ea8cfb6583e33a
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-12-22 14:07:20 +0300

    Created logger

---
+++ backend/public/config/common/slim.php
@@ -0,0 +1,1 @@
+<?phpdeclare(strict_types=1);use Psr\Container\ContainerInterface;use Slim\CallableResolver;use Slim\Interfaces\CallableResolverInterface;return [    CallableResolverInterface::class => static function (ContainerInterface $container): CallableResolverInterface {        return new CallableResolver($container);    },];
--- backend/public/src/Http/Action/HomeAction.php
+++ backend/public/src/Http/Action/HomeAction.php
@@ -1,7 +1,7 @@
-<?php
+<?phpdeclare(strict_types=1);namespace App\Http\Action;use App\Http\JsonResponse;use Exception;use Psr\Http\Message\ResponseInterface;use Psr\Http\Message\ServerRequestInterface;use Psr\Http\Server\RequestHandlerInterface;use stdClass;class HomeAction implements RequestHandlerInterface{    public function handle(ServerRequestInterface $request): ResponseInterface    {        return new JsonResponse(new stdClass());    }}
+namespace App\Http\Action;
 
-declare(strict_types=1);
-
+use App\Http\JsonResponse;
 namespace App\Http\Action;
 
 use App\Http\JsonResponse;
