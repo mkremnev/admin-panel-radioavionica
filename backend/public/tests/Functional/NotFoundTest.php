c5c40228d82a1402ae501aceb4e7253c3465eee7
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-12-22 21:55:38 +0300

    Refactoring code

--- backend/public/tests/Functional/NotFoundTest.php
+++ backend/public/tests/Functional/NotFoundTest.php
@@ -1,17 +1,1 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Test\Functional;
-
-use Test\Functional\WebTestCase;
-
-class NotFoundTest extends WebTestCase
-{
-    public function testNotFound(): void
-    {
-        $response = $this->app()->handle(self::json('GET', '/not-found'));
-
-        self::assertEquals(404, $response->getStatusCode());
-    }
-}
+<?phpdeclare(strict_types=1);namespace Test\Functional;use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;class NotFoundTest extends WebTestCase{    use ArraySubsetAsserts;    public function testNotFound(): void    {        $response = $this->app()->handle(self::json('GET', '/not-found'));        self::assertEquals(404, $response->getStatusCode());        self::assertJson($body = (string)$response->getBody());        self::assertArraySubset([            'message' => '404 Not Found'        ], Json::decode($body));    }}
