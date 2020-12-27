308cf1bfbc04214728210612a0ed08d68c294302
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-12-27 17:37:43 +0300

    Refactor Entity User and updated controllers

--- backend/public/src/Auth/Entity/User/Token.php
+++ backend/public/src/Auth/Entity/User/Token.php
@@ -1,70 +1,1 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Auth\Entity\User;
-
-use Doctrine\ORM\Mapping as ORM;
-use DomainException;
-use DateTimeImmutable;
-use Webmozart\Assert\Assert;
-
-/**
- * @ORM\Embeddable
- */
-class Token
-{
-    /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
-     */
-    private $value;
-    /**
-     * @var DateTimeImmutable
-     * @ORM\Column(type="datetime_immutable", nullable=true)
-     */
-    private DateTimeImmutable $expires;
-
-    public function __construct(string $value, DateTimeImmutable $expires)
-    {
-        Assert::uuid($value);
-        $this->value = mb_strtolower($value);
-        $this->expires = $expires;
-    }
-
-    public function validate(string $value, DateTimeImmutable $date): void
-    {
-        if (!$this->isEqualTo($value)) {
-            throw new DomainException("Token is invalid");
-        }
-
-        if ($this->isExpiredTo($date)) {
-            throw new DomainException("Token is expired");
-        }
-    }
-
-    private function isEqualTo(string $value): bool
-    {
-        return $this->value === $value;
-    }
-
-    public function isExpiredTo(DateTimeImmutable $value): bool
-    {
-        return $this->expires <= $value;
-    }
-
-    public function getValue(): string
-    {
-        return $this->value;
-    }
-
-    public function getExpires(): DateTimeImmutable
-    {
-        return $this->expires;
-    }
-
-    public function isEmpty(): bool
-    {
-        return empty($this->token);
-    }
-}
+<?phpdeclare(strict_types=1);namespace App\Auth\Entity\User;use Doctrine\ORM\Mapping as ORM;use DomainException;use DateTimeImmutable;use Webmozart\Assert\Assert;/** * @ORM\Embeddable */class Token{    /**     * @var string     * @ORM\Column(type="string", nullable=true)     */    private $value;    /**     * @var DateTimeImmutable     * @ORM\Column(type="datetime_immutable", nullable=true)     */    private DateTimeImmutable $expires;    public function __construct(string $value, DateTimeImmutable $expires)    {        Assert::uuid($value);        $this->value = mb_strtolower($value);        $this->expires = $expires;    }    public function validate(string $value, DateTimeImmutable $date): void    {        if (!$this->isEqualTo($value)) {            throw new DomainException("Token is invalid");        }        if ($this->isExpiredTo($date)) {            throw new DomainException("Token is expired");        }    }    private function isEqualTo(string $value): bool    {        return $this->value === $value;    }    public function isExpiredTo(DateTimeImmutable $value): bool    {        return $this->expires <= $value;    }    public function getValue(): string    {        return $this->value;    }    public function getExpires(): DateTimeImmutable    {        return $this->expires;    }    public function isEmpty(): bool    {        return empty($this->value);    }}
