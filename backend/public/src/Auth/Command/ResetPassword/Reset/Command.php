c72629f97deeff006d09e1d2e5ece211b3af2744
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-01-11 19:39:02 +0300

    Created ResetPassword action and tests for him

--- backend/public/src/Auth/Command/ResetPassword/Reset/Command.php
+++ backend/public/src/Auth/Command/ResetPassword/Reset/Command.php
@@ -1,11 +1,1 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Auth\Command\ResetPassword\Reset;
-
-class Command
-{
-    public string $token = '';
-    public string $password = '';
-}
+<?phpdeclare(strict_types=1);namespace App\Auth\Command\ResetPassword\Reset;use Symfony\Component\Validator\Constraints as Assert;class Command{    /**     * @Assert\NotBlank     */    public string $token = '';    /**     * @Assert\NotBlank     * @Assert\Length(     *     min=10,     *     minMessage="Длина пароля не должна быть меньше {{ limit }} символов.",     *     allowEmptyString=true     * )     */    public string $password = '';}
