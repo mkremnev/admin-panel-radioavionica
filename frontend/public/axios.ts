ae369df1b063240c88e21a82d98be2d716df4eb2
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-01-12 14:45:25 +0300

    Created docker node frontend

--- frontend/public/axios.ts
+++ frontend/public/axios.ts
@@ -1,17 +1,19 @@
 import axios from 'axios';
+import { AxiosInstance } from 'axios';
 
-const baseURL = 'http://localhost:8888',
+const baseURL = 'http://localhost:8081',
 	timeout = 6000;
 
 const tzoffset = new Date().getTimezoneOffset();
 
-const ax = axios.create({
+const ax: AxiosInstance = axios.create({
 	baseURL,
 	timeout,
-	hedders: {
+	headers: {
 		'Content-Type': 'application/json',
-		'Local-Timezone-offset': -tzoffset
-	}
+		'Local-Timezone-offset': -tzoffset,
+		'Accept-Language': 'ru',
+	},
 });
 
 export { ax as axios };
